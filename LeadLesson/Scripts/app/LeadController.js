3926c8743da1e28611717f4187159bd73765b85b
igiel <bartlomiej.igla@gmail.com>
2015-10-14 10:25:33 +0200

    Next and prev button added. UI and UX enchancements

--- LeadLesson/Scripts/app/LeadController.js
+++ LeadLesson/Scripts/app/LeadController.js
@@ -4,8 +4,10 @@
     var LeadController = function ($scope, $leadExamples) {
         var LeadDistributionDict = $leadExamples;
 
+        $scope.canProceedFurther = false;
+        $scope.canProceedBack = false;
         $scope.currentPage = 0;
-        $scope.pageSize = 5;
+        $scope.pageSize = 7;
 
         $scope.LeadDistribution = LeadDistributionDict;
 
@@ -24,14 +26,43 @@
             
             if(allExamplesCorrect && $scope.LeadDistribution.length <= startValue + $scope.pageSize)
             {
-                $scope.validationMessage = 'Well done! You did it all corect. Now it is the time to send it to your partner.';
+                $scope.validationMessage = 'Well done! You did it all corect. Now it is the time to send it to your partner and compare your scores.';
                 return;
             }
-            if (allExamplesCorrect && $scope.LeadDistribution.length > startValue )
-                $scope.currentPage++;
             
-            $scope.validationMessage = allExamplesCorrect ? 'Well done! And those?' : 'Are you sure?';
+            if(allExamplesCorrect)
+            {
+                $scope.validationMessage = 'Well done! Would it be so easy also for a next set?';
+                $scope.canProceedFurther = allExamplesCorrect;
+            }
+            else
+                $scope.validationMessage = 'Are you sure?';
         };
+
+        $scope.proceedToTheNext = function () {
+            $scope.currentPage++;
+            setProceedBackAndFurther();
+        }
+
+        $scope.proceedBack = function () {
+            $scope.currentPage--;
+            setProceedBackAndFurther();
+        }
+        
+        var setProceedBackAndFurther = function () {
+            $scope.canProceedBack = $scope.currentPage > 0;
+
+            var startValue = $scope.currentPage * $scope.pageSize;
+            var endValue = $scope.LeadDistribution.length > startValue + $scope.pageSize ? startValue + $scope.pageSize : $scope.LeadDistribution.length;
+
+            var allExamplesCorrect = true;
+            for (var i = startValue; i < endValue; i++) {
+                var distribution = $scope.LeadDistribution[i];
+                if (!distribution.correct)
+                    allExamplesCorrect = false;
+            }
+            $scope.canProceedFurther = allExamplesCorrect && $scope.LeadDistribution.length >= endValue;
+        }
     };
 
     LeadController.$inject = ['$scope', 'leadExamples'];
