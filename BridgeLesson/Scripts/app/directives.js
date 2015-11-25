65020c6bb65711e750b9ec9ed1cca838b0775162
igiel <bartlomiej.igla@gmail.com>
2015-11-25 20:13:06 +0100

    cshtml :)

--- BridgeLesson/Scripts/app/directives.js
+++ BridgeLesson/Scripts/app/directives.js
@@ -2,28 +2,28 @@
     angular.module('BridgeLessonModule').directive('biddingQuiz', function() {
         return {
             restrict: 'E',
-            templateUrl: '/Scripts/templates/bidding-quiz.html'
+            templateUrl: '/Scripts/templates/bidding-quiz.cshtml'
         };
     });
 
     angular.module('BridgeLessonModule').directive('biddingQuizResults', function() {
         return {
             restrict: 'E',
-            templateUrl: '/Scripts/templates/bidding-quiz-results.html'
+            templateUrl: '/Scripts/templates/bidding-quiz-results.cshtml'
         };
     });
 
     angular.module('BridgeLessonModule').directive('addNewSystemControl', function() {
         return {
             restrict: 'E',
-            templateUrl: '/Scripts/templates/add-new-system-control.html'
+            templateUrl: '/Scripts/templates/add-new-system-control.cshtml'
         };
     });
 
     angular.module('BridgeLessonModule').directive('addNewBiddingSequence', function () {
         return {
             restrict: 'E',
-            templateUrl: '/Scripts/templates/add-new-bidding-sequence.html'
+            templateUrl: '/Scripts/templates/add-new-bidding-sequence.cshtml'
         };
     });
 
